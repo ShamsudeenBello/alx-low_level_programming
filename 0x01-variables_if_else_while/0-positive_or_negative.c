405366cdeaa096f2ff66b1ac20bce1609952b51a
ShamsudeenBello <Shamsubello700@gmail.com>
2022-04-08 22:39:46 +0000

    Random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,7 +15,7 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 
-if (n==0)
+if ( n==0 )
 {
 printf("%i is zero\n", n);
 }
@@ -31,4 +31,5 @@ printf("%i is positive\n", n);
 }
 
 return (0);
+
 }
