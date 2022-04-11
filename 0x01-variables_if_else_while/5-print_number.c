9c890346b554cc89c247b764785eea4037b429e2
ShamsudeenBello <Shamsubello700@gmail.com>
2022-04-11 15:19:09 +0000

    Print digits

---
+++ 0x01-variables_if_else_while/5-print_number.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * 
+ * Return: Always 0 (sucess/correct)
+ */
+int main(void)
+{
+int digit;
+
+for (digit = o; digit < 10; digit++)
+{
+ptrintf("%digit", digit);
+}
+
+printf("\n");
+
+return (0);
+
+}
