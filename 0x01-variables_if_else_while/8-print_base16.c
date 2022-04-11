d53647cfe16615405436522e45cc6486b13bb9a8
ShamsudeenBello <Shamsubello700@gmail.com>
2022-04-11 21:23:47 +0000

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,21 +5,20 @@
  * Return: Always return 0 (sucess / correct)
  */
 int main(void)
-
 {
 char base_16;
 
-for (base_16 = 48; base_16 <= 57; base_16++)
+for (base_16 = 48 ; base_16 <= 57; base_16++)
 {
 putchar(base_16);
 }
 
-for (base_16 = 97; base_16 <= 102; base_16++)
+for (base_16 = 97 ; base_16 <= 102; base_16++)
 {
 putchar(base_16);
 }
 
-putchar ('\n');
+putchar('\n');
 
 return (0);
 }
