a362c8bcfde491fee661d8bb6cc7a3a77513062d
ShamsudeenBello <Shamsubello700@gmail.com>
2022-04-12 15:26:04 +0000

    prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char ermias[] = "_putchar";
+
+	int c;
+
+	for (c = 0; c < 8; c++)
+	{
+		_putchar(ermias[]);
+	}
+	_putchar('\n');
+
+	return (0);
+}
