d76cf4cf2345ecd6cc24f909bec7fb273471eabf
ShamsudeenBello <Shamsubello700@gmail.com>
2022-04-13 13:31:19 +0000

    my commit message

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -6,21 +6,22 @@
  */
 int main(void)
 {
-	int i;
+int i;
 
-	printf("Infinite loop incoming :(\n");
+printf("Infinite loop incoming :(\n");
 
-	i = 0;
+i = 0;
 
-	/*
-	 * while (i < 10)
-	 * {
-	 * Infinite loop - No increment of variable
-	 * putchar(i);
-	 }
-	 */
+/*
+ * while (i < 10)
+ * {
+ * Infinite loop - No increment of variable
+ * putchar(i);
+}
+*/
 
-	printf("Infinite loop avoided! \\o/\n");
-	return (0);
+printf("Infinite loop avoided! \\o/\n");
+
+return (0);
 } 
 
