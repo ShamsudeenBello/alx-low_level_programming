6725a46e26f79a48f57f828f9b60fe8bbcb4557c
ShamsudeenBello <Shamsubello700@gmail.com>
2022-04-14 15:57:26 +0000

    my commit message

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+<#include <stdio.h>
+/**
+ * main - prime factors of 612852475143
+ *
+ * Return: int
+ */
+
+int main(void)
+{
+	assigned long n, i, max;
+
+	n = 612852475143;
+	max = i;
+
+	for (i = 3; i <= n; +=2)
+	{
+		while (n % i == 0)
+		{
+			max = n;
+			n = n / i;
+		}
+	}
+	printf("%io/n", max);
+	return (0);
+}
