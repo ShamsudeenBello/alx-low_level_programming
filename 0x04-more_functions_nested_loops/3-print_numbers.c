26a7964960844ad12e2dcbadfa59d0f581276526
ShamsudeenBello <Shamsubello700@gmail.com>
2022-04-14 10:53:34 +0000

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers from 0 to 9
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	i = 0;
+	while (i < 10)
+	{
+		_putchar (i + '0')
+			i++;
+	}
+
+	_putchar ('\n')';
+
+	return (0);
+}
