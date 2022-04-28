8cdf3e8c409b36dc77f7a439030949daa80ab58d
ShamsudeenBello <Shamsubello700@gmail.com>
2022-04-28 11:20:13 +0000

    reverse string

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,13 +2,15 @@
 /**
  * _print_rev_recursion - function that prints a string in reverse
  * @s: parameter string
- *
+ * Return: Always 0
  */
 
 void print_rev_recursion(char *s)
 {
 	if(*s != '\0')
 	{
+		return;
+
 		_print_rev_recursion(s + 1);
 		_putchar(*s);
 	}
